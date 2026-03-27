aa4c8c36fb39e32e94d92405c4a22bb46c7319bf
leeesooon <ewleson@163.com>
2026-03-27 16:10:57 +0800

    fix: load packaged renderer from dist-renderer

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import renderer from 'vite-plugin-electron-renderer'
 import path from 'path'
 
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist-renderer',
+  },
   plugins: [
     react(),
     electron([
